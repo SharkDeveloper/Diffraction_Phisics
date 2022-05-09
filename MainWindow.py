a8f2fbe6a9b27e081151ff3bf200368c0dba153d
SharkDeveloper <pvalera336@gmail.com>
2022-05-09 21:47:47 +0300

    dif_for_grid

--- MainWindow.py
+++ MainWindow.py
@@ -98,7 +98,7 @@ class Ui_MainWindow(object):
         self.label_2.setObjectName(u"label_2")
         self.label_2.setAlignment(Qt.AlignCenter)
 
-        self.horizontalLayout_2.addWidget(self.label_2)
+        self.horizontalLayout_2.addWidget(self.label_2, 0, Qt.AlignLeft)
 
         self.lineEdit = QLineEdit(self.frame_4)
         self.lineEdit.setObjectName(u"lineEdit")
@@ -106,6 +106,11 @@ class Ui_MainWindow(object):
         self.lineEdit.setSizePolicy(sizePolicy1)
 
         self.horizontalLayout_2.addWidget(self.lineEdit)
+
+        self.label_5 = QLabel(self.frame_4)
+        self.label_5.setObjectName(u"label_5")
+
+        self.horizontalLayout_2.addWidget(self.label_5, 0, Qt.AlignLeft)
 
         self.pushButton = QPushButton(self.frame_4)
         self.pushButton.setObjectName(u"pushButton")
@@ -122,15 +127,11 @@ class Ui_MainWindow(object):
         self.frame_3.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_3 = QHBoxLayout(self.frame_3)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.label_5 = QLabel(self.frame_3)
-        self.label_5.setObjectName(u"label_5")
-        sizePolicy.setHeightForWidth(self.label_5.sizePolicy().hasHeightForWidth())
-        self.label_5.setSizePolicy(sizePolicy)
-        self.label_5.setMinimumSize(QSize(200, 0))
-        self.label_5.setMaximumSize(QSize(16777215, 16777215))
-        self.label_5.setPixmap(QPixmap(u"image/img-qo4c_Q.png"))
+        self.label_9 = QLabel(self.frame_3)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setPixmap(QPixmap(u"image/img-qo4c_Q.png"))
 
-        self.horizontalLayout_3.addWidget(self.label_5)
+        self.horizontalLayout_3.addWidget(self.label_9)
 
         self.frame = QFrame(self.frame_3)
         self.frame.setObjectName(u"frame")
@@ -181,10 +182,109 @@ class Ui_MainWindow(object):
         self.tabWidgetPage2.setMaximumSize(QSize(16777215, 16777215))
         self.verticalLayout_2 = QVBoxLayout(self.tabWidgetPage2)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.graphicsView_2 = QChartView(self.tabWidgetPage2)
-        self.graphicsView_2.setObjectName(u"graphicsView_2")
+        self.frame_6 = QFrame(self.tabWidgetPage2)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setFrameShape(QFrame.StyledPanel)
+        self.frame_6.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_4 = QVBoxLayout(self.frame_6)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.frame_10 = QFrame(self.frame_6)
+        self.frame_10.setObjectName(u"frame_10")
+        self.frame_10.setFrameShape(QFrame.StyledPanel)
+        self.frame_10.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_6 = QHBoxLayout(self.frame_10)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.L = QLabel(self.frame_10)
+        self.L.setObjectName(u"L")
 
-        self.verticalLayout_2.addWidget(self.graphicsView_2)
+        self.horizontalLayout_6.addWidget(self.L)
+
+        self.lineEdit_5 = QLineEdit(self.frame_10)
+        self.lineEdit_5.setObjectName(u"lineEdit_5")
+
+        self.horizontalLayout_6.addWidget(self.lineEdit_5)
+
+        self.b = QLabel(self.frame_10)
+        self.b.setObjectName(u"b")
+
+        self.horizontalLayout_6.addWidget(self.b)
+
+        self.lineEdit_4 = QLineEdit(self.frame_10)
+        self.lineEdit_4.setObjectName(u"lineEdit_4")
+
+        self.horizontalLayout_6.addWidget(self.lineEdit_4)
+
+        self.N = QLabel(self.frame_10)
+        self.N.setObjectName(u"N")
+
+        self.horizontalLayout_6.addWidget(self.N)
+
+        self.lineEdit_3 = QLineEdit(self.frame_10)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+
+        self.horizontalLayout_6.addWidget(self.lineEdit_3)
+
+        self.pushButton_2 = QPushButton(self.frame_10)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.horizontalLayout_6.addWidget(self.pushButton_2)
+
+
+        self.verticalLayout_4.addWidget(self.frame_10)
+
+        self.frame_8 = QFrame(self.frame_6)
+        self.frame_8.setObjectName(u"frame_8")
+        self.frame_8.setFrameShape(QFrame.StyledPanel)
+        self.frame_8.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_8)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.label_8 = QLabel(self.frame_8)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setMaximumSize(QSize(16777215, 99999))
+        self.label_8.setPixmap(QPixmap(u"image/\u0434\u0438\u0444\u0440\u0435\u04482.jpg"))
+        self.label_8.setScaledContents(True)
+
+        self.horizontalLayout_4.addWidget(self.label_8, 0, Qt.AlignHCenter|Qt.AlignVCenter)
+
+        self.frame_9 = QFrame(self.frame_8)
+        self.frame_9.setObjectName(u"frame_9")
+        self.frame_9.setFrameShape(QFrame.StyledPanel)
+        self.frame_9.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_5 = QVBoxLayout(self.frame_9)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.graphicsView_2 = QChartView(self.frame_9)
+        self.graphicsView_2.setObjectName(u"graphicsView_2")
+        self.graphicsView_2.setMouseTracking(True)
+
+        self.verticalLayout_5.addWidget(self.graphicsView_2)
+
+        self.frame_7 = QFrame(self.frame_9)
+        self.frame_7.setObjectName(u"frame_7")
+        self.frame_7.setFrameShape(QFrame.StyledPanel)
+        self.frame_7.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_5 = QHBoxLayout(self.frame_7)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_7 = QLabel(self.frame_7)
+        self.label_7.setObjectName(u"label_7")
+
+        self.horizontalLayout_5.addWidget(self.label_7)
+
+        self.label_6 = QLabel(self.frame_7)
+        self.label_6.setObjectName(u"label_6")
+
+        self.horizontalLayout_5.addWidget(self.label_6)
+
+
+        self.verticalLayout_5.addWidget(self.frame_7)
+
+
+        self.horizontalLayout_4.addWidget(self.frame_9)
+
+
+        self.verticalLayout_4.addWidget(self.frame_8)
+
+
+        self.verticalLayout_2.addWidget(self.frame_6)
 
         self.page1.addTab(self.tabWidgetPage2, "")
 
@@ -211,12 +311,20 @@ class Ui_MainWindow(object):
         self.action.setText(QCoreApplication.translate("MainWindow", u"\u0414\u0438\u0444\u0440\u0430\u043a\u0446\u0438\u044f \u043d\u0430 \u0449\u0435\u043b\u0438", None))
         self.action_2.setText(QCoreApplication.translate("MainWindow", u"\u0414\u0438\u0444\u0440\u0430\u043a\u0446\u0438\u044f \u043d\u0430 \u043e\u0434\u043d\u043e\u0440\u043e\u0434\u043d\u043e\u0439 \u0440\u0435\u0448\u0435\u0442\u043a\u0435", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"L =", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u043c          b(\u041d\u043c) =", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u043c                                                b =", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"\u041d\u043c", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u043a\u0430\u0437\u0430\u0442\u044c", None))
-        self.label_5.setText("")
+        self.label_9.setText("")
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.page1.setTabText(self.page1.indexOf(self.Slit_page), QCoreApplication.translate("MainWindow", u"\u0414\u0438\u0444\u0440\u0430\u043a\u0446\u0438\u044f \u043d\u0430 \u0449\u0435\u043b\u0438", None))
+        self.L.setText(QCoreApplication.translate("MainWindow", u"L =", None))
+        self.b.setText(QCoreApplication.translate("MainWindow", u"\u043c                              b =", None))
+        self.N.setText(QCoreApplication.translate("MainWindow", u"\u041d\u043c                                 N =", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u043a\u0430\u0437\u0430\u0442\u044c", None))
+        self.label_8.setText("")
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.page1.setTabText(self.page1.indexOf(self.tabWidgetPage2), QCoreApplication.translate("MainWindow", u"\u0414\u0438\u0444\u0440\u0430\u043a\u0446\u0438\u044f \u043d\u0430 \u043e\u0434\u043d\u043e\u043c\u0435\u0440\u043d\u043e\u0439 \u0440\u0435\u0448\u0435\u0442\u043a\u0435", None))
     # retranslateUi
 
--- formulas.py
+++ formulas.py
@@ -30,7 +30,28 @@ def dif_for_slit(L,b):
     #print(size(x[0]))
     return x
 
-
+def dif_for_grid(L,b,N):
+    x = [[],[]] 
+    lmbd = 632.8 #Hм
+    P = 3.1416
+    if L == 0 or b == 0:
+        return 0
+    L=float(L)
+    b=float(b)
+    L*=1000000000
+    for m in range(0,100):#m*2
+        x[0].append(m*lmbd/b)
+        x[0].append(((((m+1)*lmbd/b)-(m*lmbd/b))/2)+m*lmbd/b)
+        sinus = math.sin(P*N*((m+1)+0.5))
+        #print(sinus)
+        
+        delenie = P*(m+1)+0.5
+        
+        x[1].append(pow(sinus/delenie,2))
+        x[1].append(0)
+    #print(size(x[1]))
+    #print(size(x[0]))
+    return x
 #dif_for_slit(1,2)
 
 
